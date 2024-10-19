6dc7365886244e11beacdb24f87355eeb9f76051
dsding2 <dsding2@illinois.edu>
2024-10-19 17:15:07 -0500

    ripped out all the starterpack junk

--- src/index.js
+++ src/index.js
@@ -19,15 +19,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
 // Material Dashboard 2 React Context Provider
-import { MaterialUIControllerProvider } from "context";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    <MaterialUIControllerProvider>
-      <App />
-    </MaterialUIControllerProvider>
+    <App />
   </BrowserRouter>
 );
