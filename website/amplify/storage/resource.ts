70d36c5a5e70715d4be67728efb6da3cbf4ee3e2
dsding2 <dsding2@illinois.edu>
2024-11-18 22:29:37 -0600

    test s3 perms

--- website/amplify/storage/resource.ts
+++ website/amplify/storage/resource.ts
@@ -7,7 +7,10 @@ export const storage = defineStorage({
       allow.guest.to(["read"]),
       allow.authenticated.to(["read", "write"]),
     ],
-    "code/{entity_id}/*": [allow.entity("identity").to(["read", "write"])],
+    "code/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write"]),
+      allow.authenticated.to(["read", "write"]),
+    ],
     "test_code/*": [
       allow.guest.to(["read", "write"]),
       allow.authenticated.to(["read", "write"]),
