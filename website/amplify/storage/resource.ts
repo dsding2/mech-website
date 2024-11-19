b509a2217c10fa63d9e96c3fa6ce06a56517bf3c
dsding2 <dsding2@illinois.edu>
2024-11-18 21:49:28 -0600

    change s3 permissions

--- website/amplify/storage/resource.ts
+++ website/amplify/storage/resource.ts
@@ -3,7 +3,14 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "bots",
   access: (allow) => ({
-    "results/*": [allow.guest.to(["read"])],
+    "results/*": [
+      allow.guest.to(["read"]),
+      allow.authenticated.to(["read", "write"]),
+    ],
     "code/{entity_id}/*": [allow.entity("identity").to(["read", "write"])],
+    "test_code/*": [
+      allow.guest.to(["read", "write"]),
+      allow.authenticated.to(["read", "write"]),
+    ],
   }),
 });
